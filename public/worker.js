2442c342a68779275ae947437adc6df891824c6d
Mak,Niray N.P.F <niray11@live.nl>
2020-03-26 16:32:34 +0100

    added offline page

--- public/worker.js
+++ public/worker.js
@@ -1,39 +1,37 @@
-var CACHE_NAME = 'partyuber';
+var cacheName = 'partyuber_v1';
 var urlsToCache = [
-  '/',
-  '/completed',
+    "./offline.html",
+    "./logo192.png"
 ];
 
 // Install a service worker
-self.addEventListener('install', event => {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
+self.addEventListener('install', async event => {
+    console.log("Service Worker: I am being installed, hello world!");
+    const cache = await caches.open(cacheName)
+    cache.addAll(urlsToCache)
 });
 
 // Cache and return requests
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request.url).then((response)=>{
+            return response||fetch(event.request).then((response)=>{
+                return caches.open(cacheName).then((cache)=>{
+                    return response;
+                });
+            }).catch(function(){      
+                console.log("Service Worker: Not in cache and fetching online failed, HAALLPPPP!!!",event.request);
+                if(event.request.mode==="navigate"){            
+                    return caches.match("./offline.html");
+                }
+          })
+        })
+    );
 });
 
 // Update a service worker
 self.addEventListener('activate', event => {
-  var cacheWhitelist = ['partyuber'];
+  var cacheWhitelist = ['partyuber_v1'];
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
